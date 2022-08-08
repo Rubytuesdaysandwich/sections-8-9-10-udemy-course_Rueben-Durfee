7fcbd70228213a7d0e1f507f5de07930d9d73fb7
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-08-08 11:05:23 -0600

    update

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -42,10 +42,36 @@ calcAge(1991);
 
 //Hoisting and TDZ in Practice
 
+// variables
 console.log(me);
-console.log(job);
-console.log(year);
+// console.log(job);
+// console.log(year);
 
 var me = 'jonas';
 let job = 'teacher';
 const year = 1991;
+
+//functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+function addDecl(a, b) {
+  return a + b;
+}
+const addExpr = function (a, b) {
+  return a + b;
+};
+var addArrow = (a, b) => a + b;
+
+//example
+
+if(!numProducts)
+
+
+var numProducts=10
+
+function deleteShoppingCart()[
+  console.log('all products deleted')
+]
+
