6cdb75e17b74fcdc25933c6f505e080f808d7a30
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-08-08 12:18:11 -0600

    update

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -101,7 +101,17 @@ const calcAge = function (birthYear) {
 };
 calcAge(1991);
 const calcAgeArrow = birthYear => {
+  //arrow function
   console.log(2037 - birthYear);
-  console.log(this);
+  console.log(this); //not the keyword this for this function type of global this
 };
 calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
