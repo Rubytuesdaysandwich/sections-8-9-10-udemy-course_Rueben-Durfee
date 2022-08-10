fb0c5a63de9b1c61cd324e0df92457540e0390e4
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-08-09 20:28:00 -0600

    update

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -31,24 +31,25 @@ const restaurant = {
   //   },
   // },
 };
-const arr = [2, 3, 4];
+const arr = [2, 3, 4]; //this is without destructuring
 const a = arr[0];
 const b = arr[1];
 const c = arr[2];
 
-const [x, y, z] = arr;
+//destructering arrays
+const [x, y, z] = arr; //this is an example of how a array is destructured
 console.log(x, y, z);
 console.log(arr);
 
 let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
 
-// const temp = main;
+// const temp = main;//this is the same thing as [main, secondary] = [secondary, main];
 // main = secondary;
 // secondary = temp;
 // console.log(main, secondary);
 
-[main, secondary] = [secondary, main];
+[main, secondary] = [secondary, main]; //using this method you don't need the temporary variable
 console.log(main, secondary);
 
 //recieve two return value from a function
@@ -56,11 +57,15 @@ const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
 //nested array inside an array
 const nested = [2, 4, [5, 6]];
-// const [i, , j] = nested;
+// const [i, , j] = nested;//* the  , , is used to skip a place in the array
 // console.log(i, j);
-const [i, , [j, k]] = nested;
+const [i, , [j, k]] = nested; //destructured to the variable nested
 console.log(i, j, k);
 
 //default values
-const [p = 1, q = 1, r = 1] = [8, 9];
+const [p = 1, q = 1, r = 1] = [8, 9]; //8 and 9 are assigned to 0 and 1 spots in the array r is undefined the [p = 1, q = 1, r = 1] are default values
 console.log(p, q, r);
+
+// destructuring objects------------------------------------
+
+//the spread operator
