1e277e1fadee06f796cd70f5aac05aa8629ab064
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-08-12 16:02:15 -0600

    update challge 1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -74,6 +74,57 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 
 GOOD LUCK 😀
 */
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+//1.
+const [players1, players2] = game.players;
+console.log(players1, players2);
+//2.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+//3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+//4.
+const players1Final = [...players1, 'Thiago', 'Coutinho'];
 
 //!=================================================
 // start logical assignment operators--------------------
