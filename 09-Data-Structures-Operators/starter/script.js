041dc7ff5dca0c8de85af0376265b3e6019ec127
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-08-12 13:51:51 -0600

    update

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -57,9 +57,22 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+//!=================================================
+// start logical assignment operators--------------------
+
+//end logical assignment operators------------------------
+
 //!==================================================
 //start nullish coalescing Operator(??)--------------------------------
+/*
+restaurant.numGuests = 0;
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
 
+//nullish: null and undefined (NOT 0 or ')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+*/
 //end nullish coalescing Operator(??)--------------------------
 //!===================================================
 //start short circuiting(&& and || )----------------------------
