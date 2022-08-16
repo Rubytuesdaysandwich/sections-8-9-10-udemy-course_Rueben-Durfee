19cb982a445bb81c93ab8f54bedaec3c1fd9232d
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-08-15 19:49:17 -0600

    es6 enhanced object literals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -5,22 +5,22 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
-const openingHours= {
-    //object
-    thu: {
-      //property of object
-      open: 12, //child property of thursday
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+const openingHours = {
+  //object
+  thu: {
+    //property of object
+    open: 12, //child property of thursday
+    close: 22,
   },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 
 const restaurant = {
   name: 'Classico Italiano',
@@ -28,13 +28,13 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-
+  //e6 enhanced object literals
+  openingHours,
 
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-//* before es6 object literals applied
+  //* before es6 object literals applied
   // openingHours: {
   //   //object
   //   thu: {
