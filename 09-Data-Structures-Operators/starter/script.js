0a97a5eacdaf6040281af9425b9f50e3b3dfb4fa
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-08-15 19:38:37 -0600

    update object literals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -5,18 +5,7 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  //order: function (starterIndex, mainIndex) {
-  // return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  // },
-
-  openingHours: {
+const openingHours= {
     //object
     thu: {
       //property of object
@@ -32,6 +21,37 @@ const restaurant = {
       close: 24,
     },
   },
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+//* before es6 object literals applied
+  // openingHours: {
+  //   //object
+  //   thu: {
+  //     //property of object
+  //     open: 12, //child property of thursday
+  //     close: 22,
+  //   },
+  //   fri: {
+  //     open: 11,
+  //     close: 23,
+  //   },
+  //   sat: {
+  //     open: 0, // Open 24 hours
+  //     close: 24,
+  //   },
+  // },
+  //* before es6 object literals applied end
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
@@ -57,7 +77,10 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+//--------------object literals
 
+//-------------- end object literals
+//!==================================================================
 // start for-of loop
 //used to loop over arrays
 
